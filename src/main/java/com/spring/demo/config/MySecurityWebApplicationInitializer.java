c2139db50db59ca819f78403b85a7d812b5f6ad1
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-04-12 11:25:04 +0530

    Created security initilizer class

---
+++ src/main/java/com/spring/demo/config/MySecurityWebApplicationInitializer.java
@@ -0,0 +1,16 @@
+/**
+ * Gagandeep
+ * 11:02:27 am
+ * 12-Apr-2020
+ */
+package com.spring.demo.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * Simple extending this class will initialize security
+ *
+ */
+public class MySecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
